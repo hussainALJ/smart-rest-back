8a0e8e517886fa051d566dd82b2bc8819dea274b
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-13 22:33:05 +0300

    Handle ZodErrors in the global errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,25 @@
+import { ZodError } from "zod";
+
 const errorHandler = (err, req, res, next) => {
   console.error("Error detected:", err.stack);
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({
+      status: "Bad request",
+      errors: err.issues.map((e) => ({
+        field: e.path.join("."),
+        status: e.code,
+        message: e.message,
+      })),
+    });
+  }
 
   const statusCode = err.statusCode || 500;
 
   res.status(statusCode).json({
     status: statusCode,
-    message: err.message || "Internal server error"
-  })
-}
+    message: err.message || "Internal server error",
+  });
+};
 
-export default errorHandler
+export default errorHandler;
