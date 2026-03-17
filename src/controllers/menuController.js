ca04f11465f94e7911eb5120a9ce2a29bc4a5d89
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-17 21:27:58 +0300

    Catch category not found err when menu item added

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -4,24 +4,33 @@ import { prisma } from "../lib/prisma.js";
 export const menuPostController = catchAsync(async (req, res, next) => {
   const { category_id, name, description, price, image_url, is_available } = req.body;
 
-  const newItem = await prisma.menuItems.create({
-    data: {
-      name,
-      description,
-      price: parseInt(price),
-      image_url,
-      is_available,
-      category: {
-        connect: {id: parseInt(category_id)}
+  try {
+    const newItem = await prisma.menuItems.create({
+      data: {
+        name,
+        description,
+        price: parseInt(price),
+        image_url,
+        is_available,
+        category: {
+          connect: {id: parseInt(category_id)}
+        },
       },
-    },
-    include: {
-      category: true
-    }
-  })
+      include: {
+        category: true
+      }
+    })
 
-  res.status(201).json({
-    status: "success",
-    data: { menuItem: newItem }
-  })
+    res.status(201).json({
+      status: "success",
+      data: { menuItem: newItem }
+    })
+  } catch (error) {
+    if (error.code === 'P2025') {
+      const err = new Error('Category not found');
+      err.statusCode = 404;
+      return next(err);
+    }
+    throw error;
+  }
 })
