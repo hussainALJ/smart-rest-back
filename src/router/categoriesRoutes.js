5079e95ab1cbf22d553192453351653463328b01
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-17 23:21:42 +0300

    Add category post endpoint with auth and schema

--- src/router/categoriesRoutes.js
+++ src/router/categoriesRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { categoriesGetController } from "../controllers/categoriesController.js";
+import { categoriesGetController, categoriesPostController } from "../controllers/categoriesController.js";
+import { authenticate } from "../middlewares/authenticate.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
+import { validate } from "../middlewares/validate.js";
+import { categorySchema } from "../validations/categoryValidation.js";
 
 const categoryRoutes = express.Router();
 
 categoryRoutes.get("/", categoriesGetController);
+categoryRoutes.post("/", authenticate, isAdmin, validate(categorySchema), categoriesPostController);
 
 export default categoryRoutes;
