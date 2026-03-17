f56f256f3fb4b15911a85dbadea8d6799be6817d
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-17 22:21:56 +0300

    Add put endpoint for menu item update

--- src/controllers/menuController.js
+++ src/controllers/menuController.js
@@ -6,7 +6,7 @@ export const menuGetController = catchAsync(async (req, res) => {
     include: { category: true },
     orderBy: { id: "asc" },
   });
- 
+
   res.status(200).json({
     status: "success",
     data: { menuItems: items },
@@ -14,7 +14,8 @@ export const menuGetController = catchAsync(async (req, res) => {
 });
 
 export const menuPostController = catchAsync(async (req, res, next) => {
-  const { category_id, name, description, price, image_url, is_available } = req.body;
+  const { category_id, name, description, price, image_url, is_available } =
+    req.body;
 
   try {
     const newItem = await prisma.menuItems.create({
@@ -25,24 +26,56 @@ export const menuPostController = catchAsync(async (req, res, next) => {
         image_url,
         is_available,
         category: {
-          connect: {id: category_id}
+          connect: { id: category_id },
         },
       },
       include: {
-        category: true
-      }
-    })
+        category: true,
+      },
+    });
 
     res.status(201).json({
       status: "success",
-      data: { menuItem: newItem }
-    })
+      data: { menuItem: newItem },
+    });
   } catch (error) {
-    if (error.code === 'P2025') {
-      const err = new Error('Category not found');
+    if (error.code === "P2025") {
+      const err = new Error("Category not found");
       err.statusCode = 404;
       return next(err);
     }
     throw error;
   }
-})
+});
+
+export const menuPutController = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const { category_id, name, description, price, image_url, is_available } = req.body;
+ 
+  try {
+    const updated = await prisma.menuItems.update({
+      where: { id: Number(id) },
+      data: {
+        ...(category_id && { category: { connect: { id: category_id } } }),
+        ...(name !== undefined && { name }),
+        ...(description !== undefined && { description }),
+        ...(price !== undefined && { price }),
+        ...(image_url !== undefined && { image_url }),
+        ...(is_available !== undefined && { is_available }),
+      },
+      include: { category: true },
+    });
+ 
+    res.status(200).json({
+      status: "success",
+      data: { menuItem: updated },
+    });
+  } catch (error) {
+    if (error.code === "P2025") {
+      const err = new Error("Menu item not found");
+      err.statusCode = 404;
+      return next(err);
+    }
+    throw error;
+  }
+});
--- src/router/menuRoutes.js
+++ src/router/menuRoutes.js
@@ -2,16 +2,18 @@ import express from "express";
 import {
   menuGetController,
   menuPostController,
+  menuPutController,
 } from "../controllers/menuController.js";
 import { menuSchema } from "../validations/menuValidation.js";
 import { validate } from "../middlewares/validate.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { isAdmin } from "../middlewares/isAdmin.js";
+import { menuUpdateSchema } from "../validations/menuUpdateValidation.js";
 
 const menuRoutes = express.Router();
 
 menuRoutes.get("/", menuGetController);
-
 menuRoutes.post("/", authenticate, isAdmin, validate(menuSchema), menuPostController);
+menuRoutes.put("/:id", authenticate, isAdmin, validate(menuUpdateSchema), menuPutController);
 
 export default menuRoutes;
