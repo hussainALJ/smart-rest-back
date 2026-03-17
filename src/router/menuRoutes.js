ecd6c3b0689e94ae532c19a9fe8a39242d6d514a
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-17 21:01:09 +0300

    Check for admin role before posting a menu item

--- src/router/menuRoutes.js
+++ src/router/menuRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 import { menuPostController } from "../controllers/menuController.js";
 import { menuSchema } from "../validations/menuValidation.js";
 import { validate } from "../middlewares/validate.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
 
 const menuRoutes = express.Router();
 
-menuRoutes.post("/", validate(menuSchema), menuPostController)
+menuRoutes.post("/", isAdmin, validate(menuSchema), menuPostController)
 
 export default menuRoutes
