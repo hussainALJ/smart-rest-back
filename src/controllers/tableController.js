e4dbadc3dfbb8828e2ea506b962ba9c5fe8918e1
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-03-19 05:20:16 +0300

    Add table creation and deletion for admin

--- src/controllers/tableController.js
+++ src/controllers/tableController.js
@@ -17,3 +17,41 @@ export const tablesGetController = catchAsync(async (req, res, next) => {
     data: { tables },
   });
 })
+
+export const tablesPostController = catchAsync(async (req, res) => {
+  const table = await prisma.tables.create({
+    data: {
+      qr_code_url: "",
+      status: "Available",
+    },
+  });
+ 
+  const menuUrl = `${process.env.FRONTEND_URL}/menu?table=${table.id}`;
+  const qrCodeDataUrl = await QRCode.toDataURL(menuUrl);
+ 
+  const updatedTable = await prisma.tables.update({
+    where: { id: table.id },
+    data: { qr_code_url: qrCodeDataUrl },
+  });
+ 
+  res.status(201).json({
+    status: "success",
+    data: { table: updatedTable },
+  });
+});
+
+export const tablesDeleteController = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+ 
+  try {
+    await prisma.tables.delete({ where: { id: Number(id) } });
+    res.status(204).send();
+  } catch (error) {
+    if (error.code === "P2025") {
+      const err = new Error("Table not found");
+      err.statusCode = 404;
+      return next(err);
+    }
+    throw error;
+  }
+});
--- src/router/tableRoutes.js
+++ src/router/tableRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { authenticate } from "../middlewares/authenticate.js";
 import { isCashier } from "../middlewares/isCashier.js";
-import { tablesGetController } from "../controllers/tableController.js";
+import {
+  tablesDeleteController,
+  tablesGetController,
+  tablesPostController,
+} from "../controllers/tableController.js";
+import { isAdmin } from "../middlewares/isAdmin.js";
 
 const tableRoutes = express.Router();
 
 tableRoutes.get("/", authenticate, isCashier, tablesGetController);
+tableRoutes.post("/", authenticate, isAdmin, tablesPostController);
+tableRoutes.delete("/:id", authenticate, isAdmin, tablesDeleteController);
 
 export default tableRoutes;
