4f074876e8ce8a14fca12ec603c4119afbb9a967
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-05-17 02:25:28 +0300

    Fix: prisma client generation issue

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,6 +1,8 @@
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import pkg from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import "dotenv/config";
+
+const { PrismaClient } = pkg;
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
